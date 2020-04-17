83c804987e7d1c36ad359c821fee2f624846e44b
Tuiuiu <lucasdario.dariolucas@gmail.com>
2020-04-16 22:19:31 -0300

    Add Gitignore and simple enemy spawner

--- ExperimentoTopDown/Assets/Scripts/SpawnController.cs
+++ ExperimentoTopDown/Assets/Scripts/SpawnController.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class SpawnController : MonoBehaviour
 {
-
+    public GameObject[] enemies;
+    public GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("summonTest", 1.0f, 3.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void summonTest()
+    {
+        Summon(enemies[0]);
+    }
+
+    private void Summon(GameObject enemyToSummon)
+    {
+        GameObject clone = Instantiate(enemyToSummon, transform.position, enemyToSummon.transform.rotation);
+        Enemy enemy = clone.GetComponent<Enemy>();
+        enemy.setTarget(player);
     }
 }
