7805ddf12953c36a71f30e45fbd666fd6bc54da3
Tuiuiu <lucasdario.dariolucas@gmail.com>
2020-04-20 18:01:17 -0300

    extract the shooting behavior from player and put it into a weapon object

--- ExperimentoTopDown/Assets/Scripts/Player/Player.cs
+++ ExperimentoTopDown/Assets/Scripts/Player/Player.cs
@@ -8,12 +8,7 @@ public class Player : MonoBehaviour
     private Rigidbody2D body;
     private Vector2 direction;
     private Vector2 lastShootDirection = Vector2.right;
-    public GameObject[] projectiles;
-
-    private float autoRate = 1.0f;
-    private float autoNext = 0f;
-
-    private int shootDamage = 3;
+    private Vector2 shootDirection = new Vector2();
 
     private bool mouseControl = true;
 
@@ -21,7 +16,6 @@ public class Player : MonoBehaviour
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        autoNext = autoRate;
     }
 
     // Update is called once per frame
@@ -31,11 +25,10 @@ public class Player : MonoBehaviour
         direction.x = Input.GetAxis("Horizontal");
         direction.y = Input.GetAxis("Vertical");
 
-        Vector2 shootDirection = new Vector2();
         if (mouseControl)
         {
-            Vector3 mapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 testDir = mapPoint - transform.position;
+            Vector3 aimPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 testDir = aimPoint - transform.position;
             shootDirection.x = testDir.x;
             shootDirection.y = testDir.y;
         }
@@ -53,44 +46,13 @@ public class Player : MonoBehaviour
                 lastShootDirection = shootDirection;
             }
         }
-
-        // Shoot when fire button is pressed
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Shoot(shootDirection);
-        }
-
-        // Upgrade fire rate of auto-weapon
-        if (Input.GetButton("Jump"))
-        {
-            upgradeFireRate();
-        }
-
-        // Update auto-weapon cooldown time, and shoot if possible
-        autoNext -= Time.deltaTime;
-        if (autoNext <= 0f)
-        {
-            Shoot(shootDirection);
-        }
-
+        
         // Move player
         body.velocity = direction.normalized * movementSpeed;
     }
 
-    private void Shoot(Vector2 shootDir)
+    public Vector2 getShootDirection()
     {
-        Vector2 shootDirection = shootDir.normalized;
-        Vector2 playerPosition = new Vector2(transform.position.x, transform.position.y);
-
-        Projectile clone = Instantiate(projectiles[0], playerPosition + shootDirection, transform.rotation).GetComponent<Projectile>();
-        // Projectile projectile = clone.GetComponent<Projectile>();
-        clone.setDirection(shootDirection);
-        clone.setDamage(shootDamage);
-        autoNext = autoRate;
-    }
-
-    private void upgradeFireRate()
-    {
-        autoRate *= 0.9f;
+        return shootDirection;
     }
 }
---
+++ ExperimentoTopDown/Assets/Scripts/Weapons/Machinegun.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Machinegun : MonoBehaviour
+{
+    public GameObject[] projectiles;
+    public GameObject player;
+    private Player playerComponent;
+
+    private float autoRate = 1.0f;
+    private float autoNext = 0f;
+
+    private int shootDamage = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        autoNext = autoRate;
+        playerComponent = player.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Shoot when fire button is pressed
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot(ShootDirection());
+        }
+
+        // Upgrade fire rate of auto-weapon
+        if (Input.GetButton("Jump"))
+        {
+            upgradeFireRate();
+        }
+        // Update auto-weapon cooldown time, and shoot if possible
+        autoNext -= Time.deltaTime;
+        if (autoNext <= 0f)
+        {
+            Shoot(ShootDirection());
+        }
+    }
+
+    private void Shoot(Vector2 shootDir)
+    {
+        Vector2 shootDirection = shootDir.normalized;
+        Vector2 weaponPosition = new Vector2(transform.position.x, transform.position.y);
+
+        Projectile clone = Instantiate(projectiles[0], weaponPosition + shootDirection, transform.rotation).GetComponent<Projectile>();
+        // Projectile projectile = clone.GetComponent<Projectile>();
+        clone.setDirection(shootDirection);
+        clone.setDamage(shootDamage);
+        autoNext = autoRate;
+    }
+
+    private void upgradeFireRate()
+    {
+        autoRate *= 0.9f;
+    }
+
+    private Vector2 ShootDirection()
+    {
+        return playerComponent.getShootDirection();
+    }
+}
