5712c8025be0b9af7541c8727d8a11e9e57198c3
Tuiuiu <lucasdario.dariolucas@gmail.com>
2020-11-17 21:04:30 -0300

    Adds small comment

--- TowerDefenseProject/Tower Defense Project/Assets/Scripts/Globals.cs
+++ TowerDefenseProject/Tower Defense Project/Assets/Scripts/Globals.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public static class Globals
 {
+    // Possible states:
+    // "idle" - "building" - "selection"
     public static string state = "idle";
     //public static bool isBuilding = false;
 }
